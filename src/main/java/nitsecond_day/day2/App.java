d42657b358f539c52d734816221e38f0bdf56c33
nitin <nitin.kumarsingh22@gmail.com>
2018-12-18 03:24:30 -0800

    test

---
+++ src/main/java/nitsecond_day/day2/App.java
@@ -0,0 +1,13 @@
+package nitsecond_day.day2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
